7aaf87d3a7b926a036ea808075add804b488a9e8
Nadim Akhtar <48604201+nadimakhtar97@users.noreply.github.com>
2021-03-27 20:20:28 +0530

    Time: 52 ms (20.71%), Space: 21.1 MB (33.54%) - LeetHub

---
+++ design-a-stack-with-increment-operation/design-a-stack-with-increment-operation.cpp
@@ -0,0 +1,51 @@
+class CustomStack {
+public:
+    
+    int* arr;
+    int top = -1;
+    int size = 0;
+    CustomStack(int maxSize) {
+      arr = new int[maxSize];
+      this->size = maxSize;
+    }
+    
+    void push(int x) {
+
+      if(top<size-1)
+      {
+        top += 1;
+        arr[top] = x;
+      }
+        
+    }
+    
+    int pop() {
+      if(top>=0)
+      {
+        return arr[top--];;
+      }else
+      {
+        return -1;
+      }
+
+        
+    }
+    
+    void increment(int k, int val) {
+        
+        for(int i=0;i<k;i++)
+        {
+          if(i<=size-1)
+          arr[i]+=val;
+        }
+
+    }
+};
+
+/**
+ * Your CustomStack object will be instantiated and called as such:
+ * CustomStack* obj = new CustomStack(maxSize);
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * obj->increment(k,val);
+ */
