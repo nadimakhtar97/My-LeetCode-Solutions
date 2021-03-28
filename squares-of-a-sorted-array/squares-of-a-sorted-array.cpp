4509162ac5f402dc92f4ea6938d7768dbb56e5c9
Nadim Akhtar <48604201+nadimakhtar97@users.noreply.github.com>
2021-03-28 19:38:59 +0530

    Time: 32 ms (60.96%), Space: 25.7 MB (99.65%) - LeetHub

---
+++ squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        
+      for(auto& a : nums)
+         a = a*a;
+      
+      sort(nums.begin(),nums.end());
+      
+      return nums;
+    }
+};
