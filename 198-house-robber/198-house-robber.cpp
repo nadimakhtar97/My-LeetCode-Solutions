23447b1d6106fb91fa5e8485b856088a0598aa32
Nadim Akhtar <48604201+nadimakhtar97@users.noreply.github.com>
2022-03-09 01:08:33 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (6.47%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,25 +1,20 @@
 class Solution {
 public:
     
-    vector<int> memo;
-    int maxLoot(int i,int& n,vector<int>& nums){
-        
-        if( i >= n)
-            return 0;
-        
-        if(memo[i] != -1)
-            return memo[i];
-        
-        int loot = max(maxLoot(i+2,n,nums)+nums[i],maxLoot(i+1,n,nums));
-        
-        return memo[i] = loot;
-    }
     
     int rob(vector<int>& nums) {
         
-        int n = nums.size();
-        memo.resize(n,-1);
-        return maxLoot(0,n,nums);
+        if( size(nums) == 1)
+            return nums[0];
+        
+        vector<int> dp(nums);
+        dp[1] = max(nums[0],nums[1]);
+        
+        for(int i=2;i<size(nums);i++){
+            dp[i] = max(dp[i-2]+nums[i],dp[i-1]);
+        }
+
+        return dp.back();
         
     }
 };
