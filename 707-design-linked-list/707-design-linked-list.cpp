d33f131a36c66cdab9d1e7a9396282c0d1ce984f
Nadim Akhtar <48604201+nadimakhtar97@users.noreply.github.com>
2022-08-10 23:47:40 +0530

    Time: 91 ms (9.85%), Space: 20 MB (13.43%) - LeetHub

---
+++ 707-design-linked-list/707-design-linked-list.cpp
@@ -0,0 +1,115 @@
+class DLL{
+    public:
+    int data;
+    DLL *next,*prev;
+    DLL(int data){
+        this->data = data;
+        next = prev = nullptr;
+    }
+};
+
+
+class MyLinkedList {
+public:
+    
+    DLL *head,*tail;
+    
+    MyLinkedList() {
+        head = new DLL(-1);
+        tail = new DLL(-2);
+        head->next = tail;
+        tail->prev = head;
+    }
+    
+    // void print(){
+    //     DLL* node = head->next;
+    //     while(node != tail){
+    //         cout<<node->data<<" ";
+    //         node = node->next;
+    //     }
+    //     cout<<endl;
+    // }
+    
+    int get(int index) {
+        // cout<<index<<": ";
+        // print();
+        DLL* node = head->next;
+        for(int i=0;i<index;i++){
+            node = node->next;
+            if(node == tail or node == nullptr){
+                return -1;
+            }
+        }
+        
+        if(node == tail)
+            return -1;
+        
+        return node->data;
+        
+    }
+    
+    void addAtHead(int val) {
+        // cout<<"addAtHead "<<val<<":";
+        
+        DLL *node = new DLL(val);
+        node->next = head->next;
+        node->prev = head;
+        node->next->prev = node->prev->next = node;
+        // print();
+        
+    }
+    
+    void addAtTail(int val) {
+        // cout<<"addAtTail "<<val<<":";
+        
+        DLL *node = new DLL(val);
+        node->next = tail;
+        node->prev = tail->prev;
+        node->next->prev = node->prev->next = node;
+        // print();
+        
+    }
+    
+    void addAtIndex(int index, int val) {
+        // cout<<"addAtIndex "<<index<<" val "<<val<<":";
+        DLL* node = head;
+        for(int i=0;i<index;i++){
+            node = node->next;
+            if(node == tail or node == nullptr){
+                return;
+            }
+        }
+        // cout<<"node->val = "<<node->data<<endl;
+        DLL* newNode = new DLL(val);
+        newNode->next = node->next;
+        newNode->prev = node;
+        newNode->next->prev = newNode->prev->next = newNode;
+        // print();
+        
+    }
+    
+    void deleteAtIndex(int index) {
+        // cout<<"deleteAtIndex "<<index<<":";
+        DLL* node = head->next;
+        for(int i=0;i<index;i++){
+            node = node->next;
+            if(node == nullptr or node == tail){
+                return;
+            }
+        }
+        
+        node->next->prev = node->prev;
+        node->prev->next = node->next;
+        // print();
+    }
+};
+
+/**
+ * Your MyLinkedList object will be instantiated and called as such:
+ * MyLinkedList* obj = new MyLinkedList();
+ * int param_1 = obj->get(index);
+ * obj->addAtHead(val);
+ * obj->addAtTail(val);
+ * obj->addAtIndex(index,val);
+ * obj->deleteAtIndex(index);
+ */
